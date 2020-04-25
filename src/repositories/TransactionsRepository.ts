8fc6522c4642475a7cfcdc3fdfe95c8a9c48ab38
Carlos Wimmer <coiote777@gmail.com>
2020-04-25 16:44:37 -0300

    List transactions with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,17 +9,39 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
-  // public all(): Transaction[] {
-  //   // TODO
-  // }
+  public all(): Transaction[] {
+    return this.transactions;
+  }
 
-  // public getBalance(): Balance {
-  //   // TODO
-  // }
+  public getBalance(): Balance {
+    const totalIncome = this.transactions.reduce(
+      (total: number, transaction) =>
+        transaction.type === 'income' ? total + transaction.value : total,
+      0,
+    );
+    const totalOutcome = this.transactions.reduce(
+      (total: number, transaction) =>
+        transaction.type === 'outcome' ? total + transaction.value : total,
+      0,
+    );
+
+    this.balance.income = totalIncome;
+    this.balance.outcome = totalOutcome;
+    this.balance.total = totalIncome - totalOutcome;
+
+    return this.balance;
+  }
 
   public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
     const transaction = new Transaction({
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -9,7 +9,12 @@ const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const result = {
+      transactions: transactionsRepository.all(),
+      balance: transactionsRepository.getBalance(),
+    };
+
+    return response.json(result);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
